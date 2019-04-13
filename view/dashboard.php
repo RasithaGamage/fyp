579e494f2c8dec71a38d5f037fd077fa025ab333
Rasitha Gamage <g.rasitha@gmail.com>
2019-04-13 16:01:15 +0530

    Dyanamically changing post area is added...Vue.js is used (similar to angualrjs)

--- view/dashboard.php
+++ view/dashboard.php
@@ -16,8 +16,11 @@ session_start();
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  
 
+   <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <script type="module">
+        import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js'
+    </script>
 
   <style>
   @media screen and (max-width: 600px) {
@@ -169,81 +172,122 @@ session_start();
           
           <!-- timeline --->
         
-              <div class="col-md-8">
-            <!-- Box Comment -->
-            <div class="card card-widget">
-              <div class="card-header">
-                <div class="user-block">
-                  <img class="img-circle" src="../dist/img/user1-128x128.jpg" alt="User Image">
-                  <span class="username"><a href="#">Jonathan Burke Jr.</a></span>
-                  <span class="description">Shared Publicly</span>
-                </div>
-                <!-- /.user-block -->
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-toggle="tooltip" title="Mark as read">
-                    <i class="fa fa-circle-o"></i></button>
-                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i>
-                  </button>
-                </div>
-                <!-- /.card-tools -->
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+              <div class="col-md-8" id="post_container">
 
-                <p>I took this photo this morning. What do you guys think?</p>
-                
-                <button type="button" class="btn btn-default btn-sm"><i class="fa fa-heart"></i> Like</button>
-                <span class="float-right text-muted">127 likes - 3 comments</span>
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer card-comments">
-                <div class="card-comment">
-                  <!-- User image -->
-                  <img class="img-circle img-sm" src="../dist/img/user3-128x128.jpg" alt="User Image">
 
-                  <div class="comment-text">
-                    <span class="username">
-                      Maria Gonzales
-                      <span class="text-muted float-right">8:03 PM Today</span>
-                    </span><!-- /.username -->
-                    It is a long established fact that a reader will be distracted
-                    by the readable content of a page when looking at its layout.
-                  </div>
-                  <!-- /.comment-text -->
-                </div>
-                <!-- /.card-comment -->
-                <div class="card-comment">
-                  <!-- User image -->
-                  <img class="img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="User Image">
+<!-- ///////////////////////////////////////  POSTS SECTION   ////////////////////////////////////////////// -->
+                  <li
+                          is="post-component"
+                          v-for="(user,index) in users"
+                          v-bind:name="user.name"
+                          v-bind:index="index"
+                  ></li>
 
-                  <div class="comment-text">
-                    <span class="username">
-                      Luna Stark
-                      <span class="text-muted float-right">8:03 PM Today</span>
-                    </span><!-- /.username -->
-                    It is a long established fact that a reader will be distracted
-                    by the readable content of a page when looking at its layout.
-                  </div>
-                  <!-- /.comment-text -->
-                </div>
-                <!-- /.card-comment -->
-              </div>
-              <!-- /.card-footer -->
-              <div class="card-footer">
-                <form action="#" method="post">
-                  <img class="img-fluid img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="Alt Text">
-                  <!-- .img-push is used to add margin to elements next to floating images -->
-                  <div class="img-push">
-                    <input type="text" class="form-control form-control-sm" placeholder="Press enter to post comment">
-                  </div>
-                </form>
-              </div>
-              <!-- /.card-footer -->
-            </div>
-            <!-- /.card -->
+                  <script>
+                   Vue.component('post-component',{
+                       template:'\
+                           <div class="card card-widget">\
+                           <div class="card-header">\
+                           <div class="user-block">\
+                           <img class="img-circle" src="../dist/img/user1-128x128.jpg" alt="User Image">\
+                           <span class="username"><a href="#">{{name}}</a></span>\
+                       <span class="description">Shared Publicly</span>\
+                   </div>\
+                   <!-- /.user-block -->\
+                   <div class="card-tools">\
+                       <button type="button" class="btn btn-tool" data-toggle="tooltip" title="Mark as read">\
+                       <i class="fa fa-circle-o"></i></button>\
+                   <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fa fa-minus"></i>\
+                       </button>\
+                       <button type="button" class="btn btn-tool" data-widget="remove"><i class="fa fa-times"></i>\
+                       </button>\
+                       </div>\
+                       <!-- /.card-tools -->\
+                       </div>\
+                       <!-- /.card-header -->\
+                       <div class="card-body">\
+                       <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">\
+\
+                       <p>I took this photo this morning. What do you guys think?</p>\
+\
+                   <button type="button" class="btn btn-default btn-sm"><i class="fa fa-heart"></i> Like</button>\
+                   <span class="float-right text-muted">127 likes - 3 comments</span>\
+                   </div>\
+                   <!-- /.card-body -->\
+                   <div class="card-footer card-comments">\
+                       <div class="card-comment">\
+                       <!-- User image -->\
+                       <img class="img-circle img-sm" src="../dist/img/user3-128x128.jpg" alt="User Image">\
+\
+                       <div class="comment-text">\
+                       <span class="username">\
+                       Maria Gonzales\
+                   <span class="text-muted float-right">8:03 PM Today</span>\
+                   </span><!-- /.username -->\
+                   It is a long established fact that a reader will be distracted\
+                   by the readable content of a page when looking at its layout.\
+                   </div>\
+                   <!-- /.comment-text -->\
+                   </div>\
+                   <!-- /.card-comment -->\
+                   <div class="card-comment">\
+                       <!-- User image -->\
+                       <img class="img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="User Image">\
+\
+                       <div class="comment-text">\
+                       <span class="username">\
+                       Luna Stark\
+                   <span class="text-muted float-right">8:03 PM Today</span>\
+                   </span><!-- /.username -->\
+                   It is a long established fact that a reader will be distracted\
+                   by the readable content of a page when looking at its layout.\
+                   </div>\
+                   <!-- /.comment-text -->\
+                   </div>\
+                   <!-- /.card-comment -->\
+                   </div>\
+                   <!-- /.card-footer -->\
+                   <div class="card-footer">\
+                       <form action="#" method="post">\
+                       <img class="img-fluid img-circle img-sm" src="../dist/img/user4-128x128.jpg" alt="Alt Text">\
+                       <!-- .img-push is used to add margin to elements next to floating images -->\
+                       <div class="img-push">\
+                       <input type="text" class="form-control form-control-sm" placeholder="Press enter to post comment">\
+                       </div>\
+                       </form>\
+                       </div>\
+                       <!-- /.card-footer -->\
+                       </div>\
+                       ',
+                       props:['name']
+                   })
+
+                      new Vue({
+                          el: '#post_container',
+                          data:{
+                              users: [
+                                  {
+                                      name: "Rasitha",
+                                      title: 'Do the dishes',
+                                  },
+                                  {
+                                      name: "Akalanka",
+                                      title: 'Do the dishes',
+                                  },
+                                  {
+                                      name: "Pasan",
+                                      title: 'Do the dishes',
+                                  }
+                              ],
+                          },
+                          methods: {
+                              updateData: function () { //this function is to update data --> users
+
+                              }
+                          }
+                      });
+                  </script>
+<!-- /////////////////////////////////////////////////////////////////////////////////////////////////////// -->
           </div>
           
             
